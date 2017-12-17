e44c79559f5b761922a227edb0b1bdf416613254
José Antonio Palacios Ramírez <joseantpalaciosramirez@gmail.com>
2017-12-17 20:35:50 +0100

    Styling with bootstrap

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React, {Component} from 'react'
+import './app.css'
+import {FormGroup, FormControl, InputGroup, Glyphicon} from 'react-bootstrap'
 
 class App extends Component{
     render(){
         return(
-            <div>
-                <div className="app-title">Music Master from App</div>
-                <div>
-                    <input placeholder="Search an artist ..."/>
-                    <button>button</button>
-                </div>
+            <div className="app">
+                <div className="app-title">Music Master</div>
+                <FormGroup>
+                    <InputGroup>
+                        <FormControl 
+                            type="text"
+                            placeholder="Search for an Artist"
+                        />
+                        <InputGroup.Addon>
+                            <Glyphicon glyph="search"></Glyphicon>
+                        </InputGroup.Addon>
+                    </InputGroup>
+                </FormGroup>
                 <div className="profile"> 
                     <div>Artis Picture</div>
                     <div>Gallery</div>
